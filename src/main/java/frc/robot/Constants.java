81b3fdb2460d6cc29afeee360b6078fec9a4b371
Coding MAIN <robovikes3067@gmail.com>
2019-02-07 15:53:42 -0600

    hi

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final int kTALON_PORT_LF = 0;
     public static final int kTALON_PORT_LB = 1;
     public static final int kTALON_PORT_RF = 2;
-    public static final int kTALON_PORT_RB = 3;
+    public static final int kTALON_PORT_RB = 10;
     public static final int kTALON_PORT_LIFT = 4;
     public static final int kTALON_PORT_PIVOT1 = 5;
     public static final int kTALON_PORT_PIVOT2 = 6;
